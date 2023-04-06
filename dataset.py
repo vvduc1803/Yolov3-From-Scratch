3353970068a603292be831ab7163ab341461311e
anaconda <vvduc03@gmail.com>
2023-04-06 11:16:39 +0700

    Finish build dataset.py

--- dataset.py
+++ dataset.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Van Duc <vvduc03@gmail.com>
+"""
+"""Import necessary packages"""
 from torch.utils.data import DataLoader, Dataset
 import os
 from PIL import Image
@@ -6,15 +11,27 @@ import config
 import numpy as np
 import torch
 from utils import iou_width_height as iou
-import matplotlib.pyplot as plt
 
 class Pascal_Data(Dataset):
+    """
+    Build custom dataset for pascal-voc dataset
+    Args:
+        img_root: root of image file (ex: data/train_val/JPEGImages)
+        annotation_root: root of annotation file (ex: data/train_val/Annotations)
+        anchors: 9 anchors use in object detection for image size (416x416)
+        class_names: name of classes (ex: car, dog, cat, ...)
+        S: 3 size of output targets in this case are 13x13, 26x26, 52x52
+        transform: transform for image and bbox
+
+    Returns:
+        img: image after transform
+        targets: tuples of 3 head after process store information of bbox, class and confident score
+    """
     def __init__(self,
                  img_root,
                  annotation_root,
                  anchors=config.ANCHORS,
                  class_names=config.PASCAL_CLASSES,
-                 image_size=416,
                  S=[13, 26, 52],
                  transform=None):
 
@@ -22,7 +39,6 @@ class Pascal_Data(Dataset):
         self.annotation_root = annotation_root
         self.annotation_files = os.listdir(annotation_root)
         self.class_names = class_names
-        self.image_size = image_size
         self.transform = transform
         self.S = S
         self.anchors = torch.tensor(anchors[0] + anchors[1] + anchors[2])  # for all 3 scales
@@ -32,28 +48,30 @@ class Pascal_Data(Dataset):
 
     def __len__(self):
         return len(self.annotation_files)
+
     def __getitem__(self, idx):
-        print(len(self.annotation_files))
+
+        # Load annotation path
         annotation_path = os.path.join(self.annotation_root, self.annotation_files[idx])
-
         tree = ET.parse(annotation_path)
-        root = tree.getroot()
-        objects = [x for x in root if 'object' in str(x)]
-        bboxes = []
-        # for object in objects:
-        #     class_name = self.class_names.index(object[0].text)
-        #     x_min, y_min, x_max, y_max = int(object[4][0].text), int(object[4][1].text), int(object[4][2].text), int(
-        #         object[4][3].text)
-        #     bboxes.append([x_min, y_min, x_max, y_max, class_name])
-        # bboxes = np.array(bboxes)
 
+        # Initialize list store bbox and label
+        bboxes = []
+
+        # Process annotation file (format: .xml)
         for elem in tree.iter():
+
+            # Take width and height of original image
             if elem.tag == 'width':
-                width = int(elem.text)
+                ori_width = int(elem.text)
             if elem.tag == 'height':
-                height = int(elem.text)
+                ori_height = int(elem.text)
+
+            # Take name of image file
             if elem.tag == 'filename':
                 img_file = elem.text
+
+            # Take bounding box and label
             if elem.tag == 'object':
                 box = []
                 for attr in list(elem):
@@ -61,28 +79,62 @@ class Pascal_Data(Dataset):
                         box.append(self.class_names.index(attr.text))
                     if attr.tag == 'bndbox':
                         for dim in list(attr):
-                            box.append(int(dim.text))
+                            if dim.tag == 'xmin':
+                                xmin = int(round(float(dim.text)))
+                            if dim.tag == 'ymin':
+                                ymin = int(round(float(dim.text)))
+                            if dim.tag == 'xmax':
+                                xmax = int(round(float(dim.text)))
+                            if dim.tag == 'ymax':
+                                ymax = int(round(float(dim.text)))
+                        box.extend([xmin, ymin, xmax, ymax])
                 bboxes.append(box)
+
+        # Roll label to the end
         bboxes = np.roll(np.array(bboxes), 4, axis=1).tolist()
 
+        # Load image
         img_path = os.path.join(self.img_root, img_file)
-        img = np.array(Image.open(img_path).convert('RGB'))/255.
+        img = np.array(Image.open(img_path).convert('RGB'))
 
+        # Apply transform for image and bboxes list
+        if self.transform:
+            augmentations = self.transform(image=img, bboxes=bboxes)
+            img = augmentations["image"]
+            bboxes = augmentations["bboxes"]
+
+        # Initialize 3 head
         targets = [torch.zeros((self.num_anchors // 3, S, S, 6)) for S in self.S]
+
+        # Load over all bounding box
         for box in bboxes:
+
+            # Take information of box
             x_min, y_min, x_max, y_max, class_label = box
-            x_min, y_min, x_max, y_max = x_min / width, y_min / height, x_max / width, y_max / height
+
+            # Normalize information
+            x_min, y_min, x_max, y_max = x_min / ori_width, y_min / ori_height, x_max / ori_width, y_max / ori_height
+
+            # Covert corners form to midpoint form
             x, y, width, height = (x_min + x_max) / 2, (y_min + y_max) / 2, x_max - x_min, y_max - y_min
 
+            # Compute iou with each anchors to choose scale
             iou_anchors = iou(torch.tensor([width, height]), self.anchors)
             anchor_indices = iou_anchors.argsort(descending=True, dim=0)
 
-            has_anchor = [False] * 3  # each scale should have one anchor
+            has_anchor = [False] * 3  # each scale should have three anchor
+
+            # Load over indices
             for anchor_idx in anchor_indices:
+
+                # Choose scale
                 scale_idx = anchor_idx // self.num_anchors_per_scale
+                S = self.S[scale_idx]
+
+                # Choose anchor in this scale
                 anchor_on_scale = anchor_idx % self.num_anchors_per_scale
 
-                S = self.S[scale_idx]
+                # Choose cell in this scale anh this anchor
                 i, j = int(S * y), int(S * x)  # which cell
                 anchor_taken = targets[scale_idx][anchor_on_scale, i, j, 0]
                 if not anchor_taken and not has_anchor[scale_idx]:
@@ -101,7 +153,6 @@ class Pascal_Data(Dataset):
                     has_anchor[scale_idx] = True
 
                 elif not anchor_taken and iou_anchors[anchor_idx] > self.ignore_iou_thresh:
-                    print(scale_idx, anchor_on_scale, i, j)
                     targets[scale_idx][anchor_on_scale, i, j, 0] = -1  # ignore prediction
 
         return img, tuple(targets)
@@ -109,16 +160,18 @@ class Pascal_Data(Dataset):
 def test():
     from utils import plot_image, cells_to_bboxes
     from utils import non_max_suppression as nms
+
     dataset = Pascal_Data('data/train_val/JPEGImages', 'data/train_val/Annotations')
-    img, y = dataset.__getitem__(100)
+    _, y = dataset.__getitem__(100)
+
     S = [13, 26, 52]
+
     scaled_anchors = torch.tensor(config.ANCHORS) / (
             1 / torch.tensor(S).unsqueeze(1).unsqueeze(1).repeat(1, 3, 2)
     )
     loader = DataLoader(dataset=dataset, batch_size=1, shuffle=True)
     for x, y in loader:
         boxes = []
-        print(x.shape)
 
         for i in range(y[0].shape[1]):
             anchor = scaled_anchors[i]
